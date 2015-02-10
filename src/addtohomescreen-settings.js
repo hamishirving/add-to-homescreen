dcae9bad78c9384774e4d3a450c90fd586a6ae88
Hamish Irving <hamishirving@Hamishs-MacBook-Pro.local>
2015-02-10 10:59:17 +0000

    Final commit for testing

--- src/addtohomescreen-settings.js
+++ src/addtohomescreen-settings.js
@@ -1,7 +1,7 @@
 addToHomescreen({
 		skipFirstVisit: true,			// show at second access
 		startDelay: 0,					// display message right away
-		lifespan: 10,					// automatically close after 5 seconds
+		lifespan: 10,					// automatically close after 10 seconds
 		displayPace: 0,					// minutes before message is shown again (null)
 		maxDisplayCount: 1 				// display message only once
 }); 
